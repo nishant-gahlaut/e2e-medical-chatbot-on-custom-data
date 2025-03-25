221f2d7c49662a4b7794593cfb9421d99e71422a
nishant-kgp <nishant.gahlaut@capillarytech.com>
2025-03-25 16:25:46 +0530

    basic setup

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="medical_chatbot",
+    packages=find_packages(),
+    version="0.0.0",
+    description="medical chatbot using custom data",
+    install_requires=[]
+)
